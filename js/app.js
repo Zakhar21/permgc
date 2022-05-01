d2d729d4a41f9122eefdb29877034c61916dd7af
Zakhar21 <ip609108@gmail.com>
2022-05-01 21:01:51 +0700

    Бургер меню - demo version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 videojs(document.querySelector('.video-js'), {
     language: 'ru',
 });
+const burger = document.querySelector('.header__burger');
+const mobMenu = document.querySelector('.mobile-menu');
+const menuClose = document.querySelector('.mobile-menu__close');
+const body = document.querySelector('body');
+burger.addEventListener('click', () => {
+    mobMenu.classList.add('active');
+    body.classList.add('overflow');
+});
+menuClose.addEventListener('click', () => {
+    mobMenu.classList.remove('active');
+    body.classList.remove('overflow');
+});
